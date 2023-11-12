d3acaef16117e906d8dfc1a1ddb54d5a9d279c3f
Furkan Alan <furkanalan@hotmail.com>
2023-11-13 02:59:11 +0300

    gdignore added, moved and added example scripts

--- js/single-module.mjs
+++ js/single-module.mjs
@@ -1,16 +1,16 @@
-export default class Index extends godot.Node {
-	
+export default class SingleModule extends godot.Node {
+
 	constructor() {
 		super();
 	}
-	
+
 	// Called when the node enters the scene tree for the first time.
 	_ready() {
-		console.log("ready"); 
+		console.log("Single module is ready");
 	}
-	
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	_process(delta) {
-		
+
 	}
 }
