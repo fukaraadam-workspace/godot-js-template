1975f5fb9c675793b533aed86bcda9bafc5b4ea4
Furkan Alan <furkanalan@hotmail.com>
2023-11-12 07:09:54 +0300

    reopening godot changes spaces to tabs in js

--- index.mjs
+++ index.mjs
@@ -1,16 +1,16 @@
 export default class Index extends godot.Node {
-    
-    constructor() {
-        super();
-    }
-    
-    // Called when the node enters the scene tree for the first time.
-    _ready() {
-        console.log("ready"); 
-    }
-    
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    _process(delta) {
-        
-    }
+	
+	constructor() {
+		super();
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	_ready() {
+		console.log("ready"); 
+	}
+	
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	_process(delta) {
+		
+	}
 }
