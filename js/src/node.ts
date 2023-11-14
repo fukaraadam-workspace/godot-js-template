44b5dac0177009ae1d30826d1221631142002e79
Furkan Alan <furkanalan@hotmail.com>
2023-11-14 13:49:20 +0300

    Add node decorator and generate random number on button press.

--- js/src/node.ts
+++ js/src/node.ts
@@ -1,15 +1,29 @@
 import { randomInteger } from './index.js';
+import { node } from './decorators';
 
 export default class ModuleWrapper extends godot.Node {
+	@node
+	label: godot.Label;
+
 	constructor() {
 		super();
 	}
 
 	// Called when the node enters the scene tree for the first time.
 	_ready() {
-		console.log('Module wrapper is ready: ', randomInteger(0, 100));
+		this.generateRandom();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	_process(delta) {}
+
+	generateRandom() {
+		const num = randomInteger(0, 100);
+		console.log('Module wrapper is ready: ', num);
+		this.label.text = '' + num;
+	}
+
+	_on_button_pressed() {
+		this.generateRandom();
+	}
 }
